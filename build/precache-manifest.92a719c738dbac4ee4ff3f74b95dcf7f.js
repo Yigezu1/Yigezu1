b03e3432c9cdd2f5a24300d3c10683a8ff86ebce
Yigezu Mulugeta <Yigezumb@gmail.com>
2021-01-10 18:28:03 -0500

    deployed to github pages

---
+++ build/precache-manifest.92a719c738dbac4ee4ff3f74b95dcf7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99266088387ee53056a638a98197da6b",
+    "url": "/yigezu1/index.html"
+  },
+  {
+    "revision": "06f6f9fa70b02431ec04",
+    "url": "/yigezu1/static/css/2.032730b7.chunk.css"
+  },
+  {
+    "revision": "d9a4beb11c0bcf812c4b",
+    "url": "/yigezu1/static/css/main.5da3225b.chunk.css"
+  },
+  {
+    "revision": "06f6f9fa70b02431ec04",
+    "url": "/yigezu1/static/js/2.238b28d3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/yigezu1/static/js/2.238b28d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a4beb11c0bcf812c4b",
+    "url": "/yigezu1/static/js/main.eecb56a8.chunk.js"
+  },
+  {
+    "revision": "377fefbdf14598fbaa11",
+    "url": "/yigezu1/static/js/runtime-main.5e13a32d.js"
+  }
+]);
